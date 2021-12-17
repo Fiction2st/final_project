b164a0caeeb800c75c5cdd697abe7f2056b8b8b8
Fiction2st <babu6322@naver.com>
2021-12-17 23:44:29 +0900

    kakao login api branch 수정완료

--- src/main/java/com/hntrip/root/member/controller/MemberController.java
+++ src/main/java/com/hntrip/root/member/controller/MemberController.java
@@ -17,7 +17,6 @@ import javax.servlet.http.HttpSession;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,9 +29,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.hntrip.root.common.session.MemberSessionName;
 import com.hntrip.root.member.dto.MemberDTO;
 import com.hntrip.root.member.service.MemberService;
